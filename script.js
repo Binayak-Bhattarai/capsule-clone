d88d3a1858e99a67621976861c6f966ed88be680
Binayak-Bhattarai <binayakbhattaraiofficial.99@gmail.com>
2025-06-04 10:38:19 +0545

    js updated

--- script.js
+++ script.js
@@ -1,34 +1,47 @@
-function preloader(){
+function preloader() {
+
+
   var tl = gsap.timeline();
 
-tl.to("#loader", {
-  width: "100%",
-  duration: 1.3,
-  delay: 0.5,
-  ease: "circ.inOut",
-  onComplete: () => {
-    document.querySelector("#capsule h3").textContent = "";
-    document.querySelector("#loader").style.backgroundColor = "transparent";
-    
-    
-  }
-});
-   
-    
+  tl.to("#loader", {
+    width: "100%",
+    duration: 1.3,
+    delay: 0.5,
+    ease: "circ.inOut",
+    onComplete: () => {
+      document.querySelector("#capsule h3").textContent = "";
+      document.querySelector("#loader").style.backgroundColor = "transparent";
 
-tl.to("#capsule", {
-    
-  height: "100%",
-  width: "100%",
-  duration: 0.7,
+
+    }
+  });
+
+
+
+  tl.to("#capsule", {
+
+    height: "100%",
+    width: "100%",
+    duration: 0.7,
     ease: "power3.out",
-  onComplete: () => {
-   document.querySelector("#preloader").style.display = "none";
+    onComplete: () => {
+      document.querySelector("#preloader").style.display = "none";
 
-  }
+    }
 
-});
+  });
+
+  tl.from("#page1 h1", {
+    y: 50,
+    autoAlpha: 0,
+    duration:0.7,
+    ease: "circ.inOut",
+  })
+
+  
 }
+
+
 
 preloader();
 
